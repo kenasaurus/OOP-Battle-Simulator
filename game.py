e8917e989f426bdbf87370b33d93253aeff95715
kenasaurus <khenzinugent@gmail.com>
2026-09-14 11:57:34 -0500

    build hero class

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 from goblin import Goblin
-
+from hero import Hero
 
 ARENA_NAME = "Underground Arena"
 
@@ -9,15 +9,23 @@ def main():
     print(f"Welcome to {ARENA_NAME}!")
     print("༼ ᓄºل͟º ༽ᓄ   ᕦ(ò_óˇ)ᕤ")
     print("The gates are opening...")
+    print("WHO WILL BE PREPARED TO BATTLE TO THE DEATH")
 
     goblin = Goblin("Gribble")
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
 
-  goblintwo = Goblin("Sribble")
+    goblintwo = Goblin("Sribble")
 
     print(f"{goblintwo.name} enters the arena with {goblintwo.health} health.")
-print("But no hero has answered the call... yet.")
+    print("But no hero has answered the call... yet.")
 
 if __name__ == "__main__":
+    Peter = Hero("Peter")
+    goblintwo = Goblin("Sribble")
+    print(f"{Peter.name} enters the arena with {Peter.health} health.")
+    heroAttack = Peter.attack()
+    goblintwo.health - heroAttack
+    Peter.take_damage(goblinAttack)
+
     main()
--- hero.py
+++ hero.py
@@ -1,4 +1,20 @@
+import random 
 class Hero:
     """The hero blueprint will be implemented later in the project."""
+    def __init__(self,name):
+        self.name=name 
+        self.health = 110
+        self.attack_power = 25
 
-    pass
+    def attack(self):
+        """return a random value from 1 through this hero's attack power"""
+        return random.randint(1, self.attack_power)
+
+    def take_damage(self, damage):
+        """Subtracts damage from the health but doesn't let health fall below 0"""
+        self.health = max(0, self.health - damage)
+        print(f"{self.name} takes {damage} damage. Health: {self.health}")
+
+    def is_alive(self):
+        """Returns a boolean to say whether the hero is alive or not."""
+        return self.health > 0
